c491a980e11ec94a3c3757839e1c308e880aea97
John Ryan Gisel <johnryangisel@Jack.local>
2018-05-20 13:04:51 -0700

    styled the application, finished the course

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -52,10 +52,10 @@ export default class ExpenseForm extends React.Component {
     };
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input 
+                <form className="form" onSubmit={this.onSubmit}>
+                    {this.state.error && <p className="form__error">{this.state.error}</p>}
+                    <input
+                        className="text-input" 
                         type="text"
                         placeholder="Description"
                         autoFocus
@@ -63,6 +63,7 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onDescriptionChange}
                     />
                     <input 
+                        className="text-input"
                         type="text"
                         placeholder="Amount"
                         value={this.state.amount}
@@ -76,14 +77,16 @@ export default class ExpenseForm extends React.Component {
                         numberOfMonths={1}
                         isOutsideRange={() => false}
                     />
-                    <textarea 
+                    <textarea
+                        className="text-area" 
                         placeholder="Add a note for your expense (optional)"
                         value={this.state.note}
                         onChange={this.onNoteChange}
                     ></textarea>
-                    <button >Add Expense</button>
+                    <div>
+                        <button className="button">Save Expense</button>
+                    </div>
                 </form>
-            </div>
         );
     }
 }
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPage = () => (
+    <div className="loader">
+        <img className="loader__img" src="/images/loader.gif" alt="Loading Image"/>
+    </div>
+);
+
+export default LoadingPage;
