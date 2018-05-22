037baf592d54cc04b250d526cf57692406de451b
John Ryan Gisel <johnryangisel@Jack.local>
2018-05-22 09:49:08 -0700

    added a not showing paragraph in the summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,7 +5,7 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-list';
 
-export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expensesCount, expensesTotal, totalExpenses }) => {
     const expensesWord = expensesCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     return (
@@ -14,6 +14,7 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
                 <h1 className="page-header__title">
                     Viewing <span>{expensesCount}</span> {expensesWord} totaling <span>{formattedExpensesTotal}</span>
                 </h1>
+                <p>There are {totalExpenses - expensesCount} expenses not showing due to the filters</p>
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -26,7 +27,8 @@ const mapStateToProp = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters)
     return {
         expensesCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses),
+        totalExpenses: state.expenses.length
     };
 };
 
